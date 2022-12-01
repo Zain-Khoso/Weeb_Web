3f05c508e190836751a4d9e1d08250b9c61b0a87
Zain <zain.khoso.dev@gmail.com>
2022-12-01 17:07:36 +0500

    Manga&AboutPage
    -Added & Designed Manga Page.
    -Added & Designed About_us Page.

--- script.js
+++ script.js
@@ -13,36 +13,12 @@ function eventHandler(node) {
             break;
 
         case "Manga":
-            document.body.innerHTML = `
-            <header>
-                <nav>
-                    <img src="assets/logo.png" alt="Logo" />
-                    <h2>Weeb Web</h2>
-                    <button class="navbtn">Sign-up</button>
-                    <button class="navbtn">About_Us</button>
-                    <button class="navbtn">Manga</button>
-                    <button class="navbtn">Anime</button>
-                    <button class="navbtn">Home</button>
-                </nav>
-            </header>
-            `;
+            document.body.innerHTML = new docStrucGenerator().manga
             eventDetector();
             break;
 
         case "About_Us":
-            document.body.innerHTML = `
-            <header>
-                <nav>
-                    <img src="assets/logo.png" alt="Logo" />
-                    <h2>Weeb Web</h2>
-                    <button class="navbtn">Sign-up</button>
-                    <button class="navbtn">About_Us</button>
-                    <button class="navbtn">Manga</button>
-                    <button class="navbtn">Anime</button>
-                    <button class="navbtn">Home</button>
-                </nav>
-            </header>
-            `;
+            document.body.innerHTML = new docStrucGenerator().about
             eventDetector();
             break;
 
@@ -116,7 +92,7 @@ function docStrucGenerator() {
         </nav>
     </header>
     <main class="animetab">
-        <div class="animetabdiv">
+        <section class="animetab">
             <h1 class="animetab">Anime?</h1>
             <h3 class="animetab">
                 Anime is hand-drawn and computer-generated animation
@@ -125,8 +101,8 @@ function docStrucGenerator() {
                 However, in Japan and in Japanese, anime describes all
                 animated works, regardless of style or origin.
             </h3>
-        </div>
-        <div class="animetabdiv">
+        </section>
+        <section class="animetab">
             <h1 class="animetab">Where to Watch Anime?</h1>
             <h3 class="animetab">
                 There are various Sourses to Watch Anime from, A few are
@@ -134,38 +110,116 @@ function docStrucGenerator() {
             </h3>
             <ol class="animetab">
                 <li class="animetab">
-                    <a href="https://www.crunchyroll.com/" class="animetab" target="_blank"
-                        >Crunchyroll</a>
+                    <a href="https://www.crunchyroll.com/" class="animetab" target="_blank">Crunchyroll</a>
                 </li>
                 <li class="animetab">
-                    <a href="https://ww3.gogoanime2.org/" class="animetab" target="_blank"
-                    >Gogo Anime</a>
+                    <a href="https://ww3.gogoanime2.org/" class="animetab" target="_blank">Gogo Anime</a>
                 </li>
                 <li class="animetab">
-                    <a href="https://9anime.vc/" class="animetab" target="_blank"
-                        >9 Anime</a>
+                    <a href="https://9anime.vc/" class="animetab" target="_blank">9 Anime</a>
                 </li>
                 <li class="animetab">
-                    <a
-                        href="https://animekaizoku.com/category/anime/"
-                        class="animetab" target="_blank"
-                        >Anime Kaizoku</a>
+                    <a href="https://animekaizoku.com/category/anime/" class="animetab" target="_blank">Anime
+                        Kaizoku</a>
                 </li>
                 <li class="animetab">
                     <a href="https://zoro.to/" class="animetab" target="_blank">Zoro</a>
                 </li>
             </ol>
-        </div>
-        <div class="animetabdiv">
-            <h1 class="animetab" id="animenews">Anime News</h1>
-            <h3 class="animetab">Click on the header to view Anime News.</h3>
-        </div>
-        <div class="animetabdiv">
+        </section>
+        <section class="animetab">
             <h1 class="animetab" id="animequots">Anime Quotes</h1>
-            <h3 class="animetab">Click on the header to view Anime Quotes.</h3>
-        </div>
+            <h3 class="animetab">
+                Click on the header to view Anime Quotes.
+            </h3>
+        </section>
+        <section class="animetab">
+            <h1 class="animetab" id="animenews">Popular Animes.</h1>
+            <h3 class="animetab">
+                Click on the header to view Animes List.
+            </h3>
+        </section>
     </main>
     `;
+
+    this.manga = `
+    <header>
+        <nav>
+            <img src="assets/logo.png" alt="Logo" />
+            <h2>Weeb Web</h2>
+            <button class="navbtn">Sign-up</button>
+            <button class="navbtn">About_Us</button>
+            <button class="navbtn">Manga</button>
+            <button class="navbtn">Anime</button>
+            <button class="navbtn">Home</button>
+        </nav>
+    </header>
+    <main class="mangatab">
+        <section class="mangatab">
+            <h1 class="mangatab">Manga?</h1>
+            <h3 class="mangatab">
+                Manga are comics or graphic novels originating from Japan. Most manga conform to a style developed in Japan in the late 19th century, and the form has a long prehistory in earlier Japanese art. The term manga is used in Japan to refer to both comics and cartooning.
+            </h3>
+        </section>
+        <section class="mangatab">
+            <h1 class="mangatab">Where to Read Manga?</h1>
+            <h3 class="mangatab">
+                There are various Sourses to Read Manga from, But one of the 
+                most trusted one is <a href="https://mangareader.to/home/" class="mangatab">Manga_Reader</a>
+            </h3>
+        </section>
+        <section class="mangatab">
+            <h1 class="mangatab" id="manganews">Popular Mangas.</h1>
+            <h3 class="mangatab">
+                Click on the header to view Manga List.
+            </h3>
+        </section>
+    </main>
+    
+    `;
+
+    this.about = `
+    <header>
+        <nav>
+            <img src="assets/logo.png" alt="Logo" />
+            <h2>Weeb Web</h2>
+            <button class="navbtn">Sign-up</button>
+            <button class="navbtn">About_Us</button>
+            <button class="navbtn">Manga</button>
+            <button class="navbtn">Anime</button>
+            <button class="navbtn">Home</button>
+        </nav>
+    </header>
+    <main class="abouttab">
+        <section class="abouttab">
+            <img src="/assets/logo.png" alt="Logo" class="abouttab logo">
+            <h1 class="abouttab">Weeb Web</h1>
+            <h3 class="abouttab">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam doloribus inventore nisi quia quo facilis iste voluptates in quidem, repudiandae nobis cumque harum eligendi, excepturi, earum iusto magni libero cum repellat veritatis odit. Quaerat voluptas distinctio ipsam. Illo repellat cumque eos officia distinctio temporibus explicabo ab facere repudiandae sapiente numquam quae asperiores provident, iure voluptas doloribus expedita, atque nesciunt assumenda! Voluptatibus facere dolores culpa nobis recusandae quo saepe hic doloribus cum quibusdam. Iusto natus optio veritatis ea non, fugiat maxime praesentium voluptatem nesciunt odit tenetur voluptate repellendus eos aliquid velit mollitia sit atque vel, eaque voluptatibus incidunt? Quis sunt id quia tempora numquam unde repellat nostrum impedit. Aliquam veritatis ut ad sint autem dolorum iure, fugit iste asperiores accusamus illo quos quia ipsa hic, nesciunt tempore quo ea eos accusantium nisi excepturi facere placeat? Sint rerum esse aspernatur laborum temporibus laboriosam, corrupti impedit? Ipsum nostrum rerum fugit expedita dolores facere nisi dolorem odit reprehenderit, cupiditate quibusdam iusto omnis doloremque nemo, ut sunt eaque rem porro corrupti hic adipisci neque maiores placeat. Facere officiis et quam perferendis vel dicta neque soluta exercitationem nemo, sed esse repudiandae molestiae magnam quisquam impedit. Pariatur, tempore? Vero iure maxime suscipit voluptas quas explicabo excepturi, delectus adipisci nemo odit mollitia ducimus iste facilis perferendis rem. Delectus, incidunt autem facilis minima quis consequatur, voluptatibus et voluptatem asperiores ratione deserunt. Placeat ex inventore corrupti dolorem! Ad ipsum delectus eligendi eum libero vitae voluptate iure repudiandae voluptatem molestias labore fugit, in magni. Nisi commodi debitis ratione, beatae, doloremque vero necessitatibus enim obcaecati magni eaque accusantium hic soluta odit optio non minima provident perferendis sunt. In exercitationem, quam dolore repellat itaque assumenda quis velit enim neque beatae asperiores voluptatibus harum, autem aliquid fuga? Est maxime incidunt dolor odit esse voluptatibus quae eaque. Laboriosam sapiente, consequuntur iure aliquam incidunt magni animi?</h3>
+        </section>
+        <section class="abouttab founder">
+            <img src="/assets/owner.png" alt="" class="abouttab founder">
+            
+            <h1 class="abouttab founder">Founder</h1>
+            
+            <div class="abouttab founder">
+                <h1 class="abouttab foundername">Z</h1>
+                <h1 class="abouttab foundername">A</h1>
+                <h1 class="abouttab foundername">I</h1>
+                <h1 class="abouttab foundername">N</h1>
+                <h1 class="abouttab foundername">-</h1>
+                <h1 class="abouttab foundername">U</h1>
+                <h1 class="abouttab foundername">L</h1>
+                <h1 class="abouttab foundername">-</h1>
+                <h1 class="abouttab foundername">A</h1>
+                <h1 class="abouttab foundername">B</h1>
+                <h1 class="abouttab foundername">D</h1>
+                <h1 class="abouttab foundername">I</h1>
+                <h1 class="abouttab foundername">N</h1>
+            </div>
+        </section>
+    </main>
+    
+    `
 }
 
 function eventDetector() {
