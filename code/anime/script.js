42fcc95db3fb83006a23869c0bc6059899cf251f
Zain <zain.khoso.dev@gmail.com>
2022-12-06 17:51:26 +0500

    New AnimePage
    -Anime Page for 2.0 update is ready, in all it's might.

--- code/anime/script.js
+++ code/anime/script.js
@@ -1,7 +1,7 @@
 function pageLoader(node) {
     switch (node.textContent) {
         case "Home":
-            open('/index.html', '_self')
+            open("/index.html", "_self");
             break;
         case "Anime":
             open("/code/anime/index.html", "_self");
@@ -18,6 +18,74 @@ function pageLoader(node) {
     }
 }
 
+function nodeGen() {
+    this.listHeader = function (name, eps) {
+        item = document.createElement("button");
+        item.innerHTML = `
+        <h1>${name},</h1>
+        <h2>${eps} Episodes.</h2>
+        `;
+        return item;
+    };
+
+    this.animeCard = function (name, eps, img, disc) {
+        let container = document.createElement("div");
+        container.id = "contextCard";
+        container.innerHTML = `
+        <div id="head">
+            <h1>Anime Details</h1>
+            <button onclick="this.parentElement.parentElement.remove()">X</button>
+        </div>
+        <div id="body">
+            <img src="${img}">
+            <h1>${name}</h1>
+            <h2>Episodes: ${eps}</h2>
+            <h3>Discription: </h3>
+            <p>${disc}</p>
+        </div>
+        `;
+        document.body.appendChild(container);
+    };
+}
+
+async function popularAnimes() {
+    try {
+        const parentNode = document.querySelector("#animelist h3");
+        const data = await (await fetch("/assets/data/animes.json")).json();
+
+        data.forEach((elem) => {
+            let dataNode = new nodeGen().listHeader(
+                elem["name"],
+                elem["episode"]
+            );
+            parentNode.appendChild(dataNode);
+        });
+    } catch (error) {
+        parentNode.textContent =
+            "We are having an issue right now, Please come back later.";
+    }
+}
+
+async function animeDetails(node) {
+    const database = await (await fetch("/assets/data/animes.json")).json();
+    const anime = node.children[0].textContent.replace(",", "");
+    const data = database.find((elem) => elem["name"] == anime);
+    new nodeGen().animeCard(
+        data.name,
+        data.episode,
+        data.img,
+        data.description
+    );
+}
+
+async function quotes() {
+    const container = document.querySelector("#quotes h3");
+    const qot = await (
+        await fetch("https://animechan.vercel.app/api/random")
+    ).json();
+    container.textContent = `"${qot["quote"]}", ~~${qot["character"]}~~ from ${qot["anime"]}`;
+}
+
 document.querySelectorAll(".navbtns").forEach((elem) => {
     elem.addEventListener("click", () => {
         pageLoader(elem);
@@ -27,10 +95,18 @@ document.querySelectorAll(".navbtns").forEach((elem) => {
 document.getElementById("sidebarbtn").addEventListener("click", () => {
     let navbar = document.getElementById("sidebar");
 
-    if (navbar.style.right == 0 || navbar.style.right == "-15rem") {
+    if (navbar.style.right == 0 || navbar.style.right == "-50rem") {
         navbar.style = `right: 0rem`;
         return;
     }
 
-    navbar.style.right = "-15rem";
+    navbar.style.right = "-50rem";
+});
+
+document.querySelector("#quotes button").addEventListener("click", quotes);
+
+popularAnimes().then(() => {
+    document.querySelectorAll("#animelist h3 button").forEach((elem) => {
+        elem.addEventListener("click", () => animeDetails(elem));
+    });
 });
